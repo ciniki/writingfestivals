0dacc73415bee8659030e531de8763d58dd14554
Andrew Rivett <veggiefrog@gmail.com>
2019-07-16 09:27:01 -0400

    Added index to mail for customer_id

---
+++ sapos/objectList.php
@@ -0,0 +1,27 @@
+<?php
+//
+// Description
+// ===========
+// This method returns the list of objects that can be returned
+// as invoice items.
+//
+// Arguments
+// =========
+// 
+// Returns
+// =======
+//
+function ciniki_writingfestivals_sapos_objectList($ciniki, $tnid) {
+
+    $objects = array(
+        //
+        // this object should only be added to carts
+        //
+        'ciniki.writingfestivals.registrations' => array(
+            'name' => 'Writing Festival Registration',
+            ),
+        );
+
+    return array('stat'=>'ok', 'objects'=>$objects);
+}
+?>
